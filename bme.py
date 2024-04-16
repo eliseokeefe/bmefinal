8b28fdade5bc89a9ed015e36a829a4ea5aa3a002
emoseswustl <e.g.moses@wustl.edu>
2024-04-16 13:16:46 -0500

    oh my

--- bme.py
+++ bme.py
@@ -14,20 +14,24 @@ from ucimlrepo import fetch_ucirepo
 heart_disease = fetch_ucirepo(id=45) 
   
 # data (as pandas dataframes) 
-print(heart_disease.data.features.dtypes)
+print(heart_disease.data.targets.isna().sum().sum())
+print(heart_disease.data)
 X = heart_disease.data.features 
 y = heart_disease.data.targets 
 
 
+from sklearn.impute import SimpleImputer
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp.fit(X)
+X = imp.transform(X)
 
-"""
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.4, random_state=42)
-rf_model = RandomForestClassifier(n_estimators=20, random_state=42)
+rf_model = RandomForestClassifier(n_estimators=7, random_state=42)
 rf_model.fit(Xtrain, ytrain)
 rf_predictions = rf_model.predict(Xtest)
 rf_accuracy = accuracy_score(ytest, rf_predictions)
 print("RandomForestClassifier model:", rf_accuracy)
-"""
+
   
 # metadata 
 # print(heart_disease.metadata) 
